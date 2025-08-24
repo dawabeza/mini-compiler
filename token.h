d18c05aaa9b474d5cd3fa8ecbb45f565ed160f8b
dawit beza <bezadawit90@gmail.com>
2025-08-24 12:19:53 +0300

    first token declaration header fully created

---
+++ token.h
@@ -0,0 +1,31 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+enum token_type {
+    //operators
+    TOKEN_PLUS, TOKEN_MINUS, TOKEN_STAR, TOKEN_SLASH, TOKEN_MODULO, TOKEN_OPEN_PARENTHESIS, TOKEN_CLOSED_PARENTHESIS,
+    TOKEN_OPEN_CURLY, TOKEN_CLOSED_CURLY, TOKEN_COMMA,
+    //keywords
+    TOKEN_IF, TOKEN_ELSE, TOKEN_FOR, TOKEN_WHILE, TOKEN_FUN, TOKEN_VAR
+};
+
+struct literal {
+    enum token_type literal_type;
+    union {
+        long double num_literal;
+        char *str_literal;
+    } lexeme;
+};
+
+struct token {
+    char *lexeme;
+    enum token_type token_type;
+    struct literal literal;
+    int start_pos;
+    int end_pos;
+    int line;
+};
+
+void init_token(struct token *token, char *lexeme, enum token_type token_type, int start_pos, int end_pos, int line);
+void destroy_token(struct token *token);
+#endif
